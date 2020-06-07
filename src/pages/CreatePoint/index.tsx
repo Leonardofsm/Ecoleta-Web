3d31281fc20c81aeca61b24fc2a9d51189922d36
Leonardo <leonardofsm@outlook.com>
2020-06-07 15:40:34 -0300

    dropzone component

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import api from '../../services/api';
 import axios from 'axios';
 import {LeafletMouseEvent} from 'leaflet';
 
+import Dropzone from '../../components/Dropzone';
+
 
 import './styles.css';
 
@@ -13,7 +15,7 @@ import logo from '../../assets/logo.svg';
 
 interface Item {
     id: number;
-    title: string;
+    name: string;
     image_url: string;
 }
 
@@ -38,6 +40,7 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const [formData, setFormData] = useState({
         name: '',
@@ -130,16 +133,20 @@ const CreatePoint = () => {
         const [ latitude, longitude ] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+
+        if (selectedFile) {
+            data.append('image', selectedFile)
+        }
 
         await api.post('points', data);
 
@@ -162,6 +169,8 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta </h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
@@ -257,8 +266,8 @@ const CreatePoint = () => {
                                 onClick={() => handleSelectItem(item.id)}
                                 className={selectedItems.includes(item.id) ? 'selected' : ''}
                                 >
-                                <img src={item.image_url} alt={item.title} />
-                                <span>{item.title}</span>
+                                <img src={item.image_url} alt={item.name} />
+                                <span>{item.name}</span>
                             </li>
                         ))}
                     </ul>
